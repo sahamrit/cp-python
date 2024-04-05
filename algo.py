05bb8dc80e86ffb8423f66bd6486a2c4d3373580
sahamrit <amritsahu.iitkgp@gmail.com>
2024-04-05 12:17:11 +0530

    added bfs

--- algo.py
+++ algo.py
@@ -1,6 +1,6 @@
 from typing import *
 
-def bin_search(arr: List, size: int) -> int:
+def bin_search(arr: List[int], size: int) -> int:
     low = 0
     high = size - 1
     while low < high:
@@ -11,3 +11,19 @@ def bin_search(arr: List, size: int) -> int:
             low = mid + 1
 
     return high
+
+def bfs(adj: List[List[int]], root: int) -> List[int]:
+    queue: List[int] = [root]
+    answer = []
+    visit = [False for _ in range(10000)]
+    visit[root] = True
+    while len(queue):
+        node = queue.pop()
+        answer.insert(0, node)
+        for nbr in adj[node]:
+            if not visit[nbr]:
+                queue.insert(0, nbr)
+                visit[nbr] = True   
+
+    answer.reverse()
+    return answer
--- test.py
+++ test.py
@@ -14,6 +14,42 @@ class Algorithms(unittest.TestCase):
                 arr[i] = True
             self.assertEqual(idx, bin_search(arr, N))
 
+    def test_bfs(self):
+        
+        # case 1
+        adj = [[] for _ in range (10)]
+        adj[0] = [1, 2, 3, 4]
+        adj[1] = [5, 6]
+        adj[2] = [7, 8]
+        adj[3] = [9]
+        adj[4] = []
+
+        self.assertEqual(list(range(10)), bfs(adj, 0))
+
+        # case 1
+        adj = [[] for _ in range (10)]
+        adj[0] = [1, 2, 3, 4]
+        adj[1] = [5, 6]
+        adj[2] = [7, 8]
+        adj[3] = [9]
+        adj[4] = []
+
+        self.assertEqual(list(range(10)), bfs(adj, 0))
+
+        adj = [[] for _ in range (10)]
+        adj[1] = [4, 5, 6]
+        adj[4]  = [3]
+        adj[5] = [2, 7]
+        
+        self.assertEqual([1, 4, 5, 6, 3, 2, 7], bfs(adj, 1))
+
+        adj = [[] for _ in range (10)]
+        adj[5] = [2, 3, 4]
+        adj[2]  = [8]
+        adj[3] = [6]
+        adj[4] = [1]
+        
+        self.assertEqual([5, 2, 3, 4, 8, 6, 1], bfs(adj, 5))
 
 if __name__ == "__main__":
     unittest.main()
